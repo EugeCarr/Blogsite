351a9d096027ebfffce88a5c221b97fe9f98050a
EugeCarr <eugecarr@gmail.com>
2023-05-17 12:17:37 +0100

    allowed host change

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-otg)rsaawu_h)o!w$!v1pedoa^68t#zejcby=py+0#5-+%+!1h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'eugenecarr.pythonanywhere.com']
 
 
 # Application definition
